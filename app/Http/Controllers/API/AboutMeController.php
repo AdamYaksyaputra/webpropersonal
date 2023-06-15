c639bd2436a1e5c048f4a9fd83484d98657995bd
Adam Yaksyaputra H <adam.yaksyaputra.hadian@gmail.com>
2023-06-15 12:31:09 +0700

    personalweb

---
+++ app/Http/Controllers/API/AboutMeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AboutMeController extends Controller
+{
+    //buatkan ro
+
+}
